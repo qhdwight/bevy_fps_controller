9fcb8a618845c860b5afc43870104e72833d4b01
qhdwight <qhdwight@gmail.com>
2022-08-08 11:09:46 -0700

    Remove default tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,3 @@ pub mod controller;
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
