c8f9146c382c9700df971b2ed320a9c0fcc3b0c8
qhdwight <qhdwight@gmail.com>
2022-11-19 22:36:54 -0500

    Update to bevy 0.9 and bevy rapier 0.19.0

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -1,23 +1,26 @@
 use std::f32::consts::TAU;
 
 use bevy::prelude::*;
+use bevy::window::CursorGrabMode;
 use bevy_rapier3d::prelude::*;
 
 use bevy_fps_controller::controller::*;
 
 fn main() {
     App::new()
-        .insert_resource(WindowDescriptor {
-            title: String::from("Minimal FPS Controller Example"),
-            ..default()
-        })
         .insert_resource(AmbientLight {
             color: Color::WHITE,
             brightness: 0.25,
         })
         .insert_resource(ClearColor(Color::rgb(0.752, 0.992, 0.984)))
         .insert_resource(RapierConfiguration::default())
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: String::from("Minimal FPS Controller Example"),
+                ..default()
+            },
+            ..default()
+        }))
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         // .add_plugin(RapierDebugRenderPlugin::default())
         .add_plugin(FpsControllerPlugin)
@@ -31,7 +34,7 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn_bundle(DirectionalLightBundle {
+    commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             illuminance: 2000.0,
             shadows_enabled: true,
@@ -46,32 +49,33 @@ fn setup(
     // The other is a "render" player that is what is displayed to the user
     // This distinction is useful for later on if you want to add multiplayer,
     // where often time these two ideas are not exactly synced up
-    commands
-        .spawn()
-        .insert(Collider::capsule(Vec3::Y * 0.5, Vec3::Y * 1.5, 0.5))
-        .insert(ActiveEvents::COLLISION_EVENTS)
-        .insert(Velocity::zero())
-        .insert(RigidBody::Dynamic)
-        .insert(Sleeping::disabled())
-        .insert(LockedAxes::ROTATION_LOCKED)
-        .insert(AdditionalMassProperties::Mass(1.0))
-        .insert(GravityScale(0.0))
-        .insert(Ccd { enabled: true }) // Prevent clipping when going fast
-        .insert(Transform::from_xyz(0.0, 3.0, 0.0))
-        .insert(LogicalPlayer(0))
-        .insert(FpsControllerInput {
+    commands.spawn((
+        Collider::capsule(Vec3::Y * 0.5, Vec3::Y * 1.5, 0.5),
+        ActiveEvents::COLLISION_EVENTS,
+        Velocity::zero(),
+        RigidBody::Dynamic,
+        Sleeping::disabled(),
+        LockedAxes::ROTATION_LOCKED,
+        AdditionalMassProperties::Mass(1.0),
+        GravityScale(0.0),
+        Ccd { enabled: true }, // Prevent clipping when going fast
+        Transform::from_xyz(0.0, 3.0, 0.0),
+        LogicalPlayer(0),
+        FpsControllerInput {
             pitch: -TAU / 12.0,
             yaw: TAU * 5.0 / 8.0,
             ..default()
-        })
-        .insert(FpsController { ..default() });
-    commands
-        .spawn_bundle(Camera3dBundle::default())
-        .insert(RenderPlayer(0));
+        },
+        FpsController { ..default() }
+    ));
+    commands.spawn((
+        Camera3dBundle::default(),
+        RenderPlayer(0),
+    ));
 
     // World
-    commands
-        .spawn_bundle(PbrBundle {
+    commands.spawn_empty()
+        .insert(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Box {
                 min_x: -20.0,
                 max_x: 20.0,
@@ -84,15 +88,15 @@ fn setup(
                 base_color: Color::hex("E6EED6").unwrap(),
                 ..default()
             }),
-            transform: Transform::identity(),
+            transform: Transform::IDENTITY,
             ..default()
         })
         .insert(Collider::cuboid(20.0, 0.25, 20.0))
         .insert(RigidBody::Fixed)
-        .insert(Transform::identity());
+        .insert(Transform::IDENTITY);
 
-    commands
-        .spawn_bundle(PbrBundle {
+    commands.spawn_empty()
+        .insert(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Box {
                 min_x: -1.0,
                 max_x: 1.0,
@@ -105,7 +109,7 @@ fn setup(
                 base_color: Color::hex("DDE2C6").unwrap(),
                 ..default()
             }),
-            transform: Transform::identity(),
+            transform: Transform::IDENTITY,
             ..default()
         })
         .insert(Collider::cuboid(1.0, 1.0, 1.0))
@@ -121,14 +125,14 @@ pub fn manage_cursor(
 ) {
     let window = windows.get_primary_mut().unwrap();
     if btn.just_pressed(MouseButton::Left) {
-        window.set_cursor_lock_mode(true);
+        window.set_cursor_grab_mode(CursorGrabMode::Locked);
         window.set_cursor_visibility(false);
         for mut controller in &mut controllers {
             controller.enable_input = true;
         }
     }
     if key.just_pressed(KeyCode::Escape) {
-        window.set_cursor_lock_mode(false);
+        window.set_cursor_grab_mode(CursorGrabMode::None);
         window.set_cursor_visibility(true);
         for mut controller in &mut controllers {
             controller.enable_input = false;
