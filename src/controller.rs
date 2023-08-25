c29daac9343301acff10a40fe71f0514ef4cee10
Joshua Maleszewski <threadsless@gmail.com>
2023-08-25 12:22:14 -0400

    moved the systems to First in the schedule

--- src/controller.rs
+++ src/controller.rs
@@ -11,7 +11,7 @@ pub struct FpsControllerPlugin;
 
 impl Plugin for FpsControllerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, (fps_controller_input, fps_controller_look, fps_controller_move, fps_controller_render).chain());
+        app.add_systems(First, (fps_controller_input, fps_controller_look, fps_controller_move, fps_controller_render).chain());
     }
 }
 
